9c4e5a15b0b2a0c0c8216e9edbe7ec445039cd2a
ssh <114060116+WWWAnonymous@users.noreply.github.com>
2022-09-21 18:40:39 +0800

    Uniform variable names

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ parser.add_argument('--bandwidth_distribution_addr', type=str, default='./data/b
 parser.add_argument('--cpu_distribution_addr', type=str, default='./data/cpu_distribution.json')
 parser.add_argument('--memory_distribution_addr', type=str, default='./data/memory_distribution.json')
 parser.add_argument('--disk_distribution_addr', type=str, default='./data/disk_distribution.json')
-parser.add_argument('--devices_out_addr', type=str, default='./out/Edge_devices_'+str(time.time())+'.csv')
+parser.add_argument('--servers_out_addr', type=str, default='./out/Edge_servers_'+str(time.time())+'.csv')
 parser.add_argument('--resource_bias_precision', type=int, default=3.5)
 parser.add_argument('--geo_bias_precision', type=int, default=0.5)
 args = parser.parse_args()
@@ -41,19 +41,19 @@ def cal_bias_resource(disk_region_list, memory_region_list, cpu_region_list, ban
     total_bias += abs(cal_spearman(memory_region_list, bandwidth_region_list) - 0.44)
     return total_bias
 
-def cal_bias_geo(GDP_data, device_resource_list):
+def cal_bias_geo(GDP_data, server_resource_list):
     GDP_list = []
     disk_list = []
     bandwidth_list = []
     memory_list = []
     cpu_list = []
-    for region in device_resource_list:
+    for region in server_resource_list:
         GDP_list.append(GDP_data[region])
-        device_list_region = device_resource_list[region]
-        disk_list.append(sum([one[0] for one in device_list_region]))
-        memory_list.append(sum([one[1] for one in device_list_region]))
-        cpu_list.append(sum([one[2] for one in device_list_region]))
-        bandwidth_list.append(sum([one[3] for one in device_list_region]))
+        server_list_region = server_resource_list[region]
+        disk_list.append(sum([one[0] for one in server_list_region]))
+        memory_list.append(sum([one[1] for one in server_list_region]))
+        cpu_list.append(sum([one[2] for one in server_list_region]))
+        bandwidth_list.append(sum([one[3] for one in server_list_region]))
     total_bias = 0
     total_bias += abs(cal_spearman(GDP_list, cpu_list) - 0.84)
     total_bias += abs(cal_spearman(cpu_list, disk_list) - 0.89)
@@ -84,27 +84,27 @@ def ESMG_step1(GDP_data):
 #         the number of edge servers in this region.
 def ESMG_step2(total_cpu, total_memory, total_disk, total_bandwidth):
     print('Step 2 - start')
-    device_num_cpu = {}
-    device_num_memory = {}
-    device_num_disk = {}
-    device_num_bandwidth = {}
-    device_num = {}
+    server_num_cpu = {}
+    server_num_memory = {}
+    server_num_disk = {}
+    server_num_bandwidth = {}
+    server_num = {}
 
-    # The total resources are divided by the average resources of a device to calculate the number of devices
+    # The total resources are divided by the average resources of a server to calculate the number of servers
     for i in total_cpu:
-        device_num_cpu[i] = total_cpu[i] / 11.51007
-        device_num_memory[i] = total_memory[i] / (17.51910 * 1024 * 1024 * 1024)
-        device_num_disk[i] = total_disk[i] / (2456.53572 * 1024 * 1024 * 1024)
-        device_num_bandwidth[i] = total_bandwidth[i] / (782.75923 * 1024 * 1024)
-        device_num[i] = int(
-            (device_num_cpu[i] + device_num_memory[i] + device_num_disk[i] + device_num_bandwidth[i]) / 4)
+        server_num_cpu[i] = total_cpu[i] / 11.51007
+        server_num_memory[i] = total_memory[i] / (17.51910 * 1024 * 1024 * 1024)
+        server_num_disk[i] = total_disk[i] / (2456.53572 * 1024 * 1024 * 1024)
+        server_num_bandwidth[i] = total_bandwidth[i] / (782.75923 * 1024 * 1024)
+        server_num[i] = int(
+            (server_num_cpu[i] + server_num_memory[i] + server_num_disk[i] + server_num_bandwidth[i]) / 4)
     print('End')
-    return device_num
+    return server_num
 
 
 # step 3: The goal of this step is to take the total number of each resource and the number of edge servers in the
 #         region as input to output the distribution of each resource in this region.
-def ESMG_step3(total_cpu, total_memory, total_disk, total_bandwidth, device_num):
+def ESMG_step3(total_cpu, total_memory, total_disk, total_bandwidth, server_num):
     print('Step 3 - start')
     disk_distribution = load_json(args.disk_distribution_addr)
     cpu_distribution = load_json(args.cpu_distribution_addr)
@@ -116,7 +116,7 @@ def ESMG_step3(total_cpu, total_memory, total_disk, total_bandwidth, device_num)
     memory_distribution_each_region = {}
     bandwidth_distribution_each_region = {}
 
-    for region in device_num:
+    for region in server_num:
         disk_distribution_tmp = []
         memory_distribution_tmp = []
         cpu_distribution_tmp = []
@@ -140,33 +140,33 @@ def ESMG_step3(total_cpu, total_memory, total_disk, total_bandwidth, device_num)
         cpu_counter = dict(Counter(cpu_distribution_adv))
         bandwidth_counter = dict(Counter(bandwidth_distribution_adv))
 
-        # Split resources to each device - disk
+        # Split resources to each server - disk
         for type in disk_counter:
-            disk_proportion = device_num[region] * (disk_counter[type] / float(len(disk_distribution_adv)))
+            disk_proportion = server_num[region] * (disk_counter[type] / float(len(disk_distribution_adv)))
             for num_same in range(int(disk_proportion)):
                 disk_distribution_tmp.append(type * disk_counter[type] / disk_proportion)
             if disk_proportion - int(disk_proportion) != 0:
                 disk_distribution_tmp.append(
                     type * disk_counter[type] / disk_proportion * (disk_proportion - int(disk_proportion)))
-        # Split resources to each device - memory
+        # Split resources to each server - memory
         for type in memory_counter:
-            memory_proportion = device_num[region] * (memory_counter[type] / float(len(memory_distribution_adv)))
+            memory_proportion = server_num[region] * (memory_counter[type] / float(len(memory_distribution_adv)))
             for num_same in range(int(memory_proportion)):
                 memory_distribution_tmp.append(type * memory_counter[type] / memory_proportion)
             if memory_proportion - int(memory_proportion) != 0:
                 memory_distribution_tmp.append(
                     type * memory_counter[type] / memory_proportion * (memory_proportion - int(memory_proportion)))
-        # Split resources to each device - cpu
+        # Split resources to each server - cpu
         for type in cpu_counter:
-            cpu_proportion = device_num[region] * (cpu_counter[type] / float(len(cpu_distribution_adv)))
+            cpu_proportion = server_num[region] * (cpu_counter[type] / float(len(cpu_distribution_adv)))
             for num_same in range(int(cpu_proportion)):
                 cpu_distribution_tmp.append(type * cpu_counter[type] / cpu_proportion)
             if cpu_proportion - int(cpu_proportion) != 0 and (type * cpu_counter[type] / cpu_proportion * (cpu_proportion - int(cpu_proportion))) != 0:
                 cpu_distribution_tmp.append(
                     type * cpu_counter[type] / cpu_proportion * (cpu_proportion - int(cpu_proportion)))
-        # Split resources to each device - bandwidth
+        # Split resources to each server - bandwidth
         for type in bandwidth_counter:
-            bandwidth_proportion = device_num[region] * (
+            bandwidth_proportion = server_num[region] * (
                     bandwidth_counter[type] / float(len(bandwidth_distribution_adv)))
             for num_same in range(int(bandwidth_proportion)):
                 bandwidth_distribution_tmp.append(type * bandwidth_counter[type] / bandwidth_proportion)
@@ -199,7 +199,7 @@ def ESMG_step3(total_cpu, total_memory, total_disk, total_bandwidth, device_num)
 def ESMG_step4(disk_distribution_each_region, memory_distribution_each_region, cpu_distribution_each_region,
                bandwidth_distribution_each_region):
     print('Step 4 - start')
-    device_resource_list = {}
+    server_resource_list = {}
     for region in disk_distribution_each_region:
         disk_region_list = disk_distribution_each_region[region]
         memory_region_list = memory_distribution_each_region[region]
@@ -284,20 +284,20 @@ def ESMG_step4(disk_distribution_each_region, memory_distribution_each_region, c
                         bandwidth_region_list = tmp_bandwidth
                         flag = 0
 
-        # Record each device attribute in the form of [disk, memory, cpu, bandwidth].
-        device_resource_tmp = []
+        # Record each server attribute in the form of [disk, memory, cpu, bandwidth].
+        server_resource_tmp = []
         for i in range(len(bandwidth_region_list)):
-            device_resource_tmp.append([disk_region_list[i], memory_region_list[i], cpu_region_list[i],
+            server_resource_tmp.append([disk_region_list[i], memory_region_list[i], cpu_region_list[i],
                                         bandwidth_region_list[i]])
-        device_resource_list[region] = device_resource_tmp
+        server_resource_list[region] = server_resource_tmp
     print('End')
-    return device_resource_list
+    return server_resource_list
 
     # step 5: The goal of this step is to take the geographic distribution of GDP/population and the edge server resource
     #         allocation in the region as input to output the geographic distribution of edge servers in this region.
-def ESMG_step5(GDP_data, device_resource_list, geographic_data):
+def ESMG_step5(GDP_data, server_resource_list, geographic_data):
     print('Step 5 - start')
-    cur_bias = cal_bias_geo(GDP_data, device_resource_list)
+    cur_bias = cal_bias_geo(GDP_data, server_resource_list)
     pre_bias = cur_bias
 
     # Following the idea of Nash equilibrium, the run is stopped when no action can reduce the
@@ -307,56 +307,56 @@ def ESMG_step5(GDP_data, device_resource_list, geographic_data):
         if flag == 1 or pre_bias<args.geo_bias_precision:
             break
         flag = 1
-        for region in device_resource_list:
-            tmp_value = device_resource_list[region]
-            for device in range(len(device_resource_list[region])):
-                for value in range(len(device_resource_list[region][device])):
-                    device_resource_list[region][device][value] = device_resource_list[region][device][value] * 0.8
-            cur_bias = cal_bias_geo(GDP_data, device_resource_list)
+        for region in server_resource_list:
+            tmp_value = server_resource_list[region]
+            for server in range(len(server_resource_list[region])):
+                for value in range(len(server_resource_list[region][server])):
+                    server_resource_list[region][server][value] = server_resource_list[region][server][value] * 0.8
+            cur_bias = cal_bias_geo(GDP_data, server_resource_list)
             if cur_bias < pre_bias:
                 pre_bias = cur_bias
                 flag = 0
             else:
-                device_resource_list[region] = tmp_value
+                server_resource_list[region] = tmp_value
 
-            tmp_value = device_resource_list[region]
-            for device in range(len(device_resource_list[region])):
-                for value in range(len(device_resource_list[region][device])):
-                    device_resource_list[region][device][value] = device_resource_list[region][device][value] * 1.2
-            cur_bias = cal_bias_geo(GDP_data, device_resource_list)
+            tmp_value = server_resource_list[region]
+            for server in range(len(server_resource_list[region])):
+                for value in range(len(server_resource_list[region][server])):
+                    server_resource_list[region][server][value] = server_resource_list[region][server][value] * 1.2
+            cur_bias = cal_bias_geo(GDP_data, server_resource_list)
             if cur_bias < pre_bias:
                 pre_bias = cur_bias
                 flag = 0
             else:
-                device_resource_list[region] = tmp_value
+                server_resource_list[region] = tmp_value
 
-    all_device = []
-    for region in device_resource_list:
-        for device in range(len(device_resource_list[region])):
-            one_device = device_resource_list[region][device]
-            one_device.append(region)
+    all_server = []
+    for region in server_resource_list:
+        for server in range(len(server_resource_list[region])):
+            one_server = server_resource_list[region][server]
+            one_server.append(region)
             # Add the latitude
-            one_device.append(random.uniform(geographic_data[region][0][0], geographic_data[region][0][1]))
+            one_server.append(random.uniform(geographic_data[region][0][0], geographic_data[region][0][1]))
             # Add the longitude
-            one_device.append(
+            one_server.append(
                 random.uniform(geographic_data[region][1][0], geographic_data[region][1][1]))
-            if int(one_device[0]/1024/1024/1024) * int(one_device[1]/1024/1024/1024) * int(one_device[2]) * int(one_device[3]/1024/1024) !=0:
-                all_device.append(one_device)
+            if int(one_server[0]/1024/1024/1024) * int(one_server[1]/1024/1024/1024) * int(one_server[2]) * int(one_server[3]/1024/1024) !=0:
+                all_server.append(one_server)
 
-    all_device_df = pd.DataFrame(
+    all_server_df = pd.DataFrame(
         {
-            'Disk (GB)': [int(device[0]/1024/1024/1024) for device in all_device],
-            'Memory (GB)': [int(device[1]/1024/1024/1024) for device in all_device],
-            'CPU (Core)': [int(device[2]) for device in all_device],
-            'Bandwidth (MB/s)': [int(device[3]/1024/1024) for device in all_device],
-            'Region': [device[4] for device in all_device],
-            'Latitude': [device[5] for device in all_device],
-            'Longitude': [device[6] for device in all_device]
+            'Disk (GB)': [int(server[0]/1024/1024/1024) for server in all_server],
+            'Memory (GB)': [int(server[1]/1024/1024/1024) for server in all_server],
+            'CPU (Core)': [int(server[2]) for server in all_server],
+            'Bandwidth (MB/s)': [int(server[3]/1024/1024) for server in all_server],
+            'Region': [server[4] for server in all_server],
+            'Latitude': [server[5] for server in all_server],
+            'Longitude': [server[6] for server in all_server]
         })
-    all_device_df.to_csv(args.devices_out_addr, encoding="utf-8-sig", mode="a", header=True, index=True)
+    all_server_df.to_csv(args.servers_out_addr, encoding="utf-8-sig", mode="a", header=True, index=True)
     print('End')
-    print('The number of successfully generated edge devices is: %d, which has been stored as file: %s.' % (len(all_device), args.devices_out_addr))
-    return all_device_df
+    print('The number of successfully generated edge servers is: %d, which has been stored as file: %s.' % (len(all_server), args.servers_out_addr))
+    return all_server_df
 
 def ESMG(GDP_addr, geographic_addr):
     # Load the data
@@ -369,20 +369,20 @@ def ESMG(GDP_addr, geographic_addr):
 
     # step 2: The goal of this step is to use the total amount of each type of resource in the region as input to output
     #         the number of edge servers in this region.
-    device_num = ESMG_step2(total_cpu, total_memory, total_disk, total_bandwidth)
+    server_num = ESMG_step2(total_cpu, total_memory, total_disk, total_bandwidth)
 
     # step 3: The goal of this step is to take the total number of each resource and the number of edge servers in the
     #         region as input to output the distribution of each resource in this region.
     disk_distribution_each_region, memory_distribution_each_region, cpu_distribution_each_region, bandwidth_distribution_each_region = ESMG_step3(
-        total_cpu, total_memory, total_disk, total_bandwidth, device_num)
+        total_cpu, total_memory, total_disk, total_bandwidth, server_num)
 
     # step 4: The goal of this step is to take the distribution of each resource in the region as input to output the
     #         resource configuration for each edge server in this region.
-    device_resource_list = ESMG_step4(disk_distribution_each_region, memory_distribution_each_region,
+    server_resource_list = ESMG_step4(disk_distribution_each_region, memory_distribution_each_region,
                                       cpu_distribution_each_region, bandwidth_distribution_each_region)
     # step 5: The goal of this step is to take the geographic distribution of GDP/population and the edge server resource
     #         allocation in the region as input to output the geographic distribution of edge servers in this region.
-    ESMG_step5(GDP_data, device_resource_list, geographic_data)
+    ESMG_step5(GDP_data, server_resource_list, geographic_data)
 
     return 0
 
